5818b805e73fc8a19d48a3de609280dfb45a7532
bemporad <abemporad@gmail.com>
2024-10-17 18:46:24 +0200

    Linear MPC approximation example working.

--- examples/example_nlmpc.py
+++ examples/example_nlmpc.py
@@ -57,31 +57,47 @@ def prediction(x, U):
     _, Y = jax.lax.scan(prediction_model, jnp.array(x), U)
     return Y
 @jax.jit
-def loss_single(U, x0, r, uold):
+def loss_single(U, x0, r, uold, integral):
     Ypred = prediction(x0, U)
-    loss = jnp.sum((Ypred-r)**2) + 0.001*((U[0]-uold)**2+jnp.sum(jnp.diff(U)**2))
+    Intpred = jnp.cumsum(Ypred-r)+integral
+    loss = jnp.sum((Ypred-r)**2) + .1*jnp.sum(Intpred**2) + 0.001*((U[0]-uold)**2+jnp.sum(jnp.diff(U)**2))
     return loss[0]
 
-loss = jax.jit(jax.vmap(loss_single, in_axes=(0,0,0,0)))
+loss = jax.jit(jax.vmap(loss_single, in_axes=(0,0,0,0,0)))
+
+@jax.jit
+def prediction_model_with_states(x, u):
+    xnext, y = prediction_model(x, u)
+    return xnext, jnp.concatenate((x.ravel(), y))
+@jax.jit
+def prediction_with_states(x, U):    
+    _, XY = jax.lax.scan(prediction_model_with_states, jnp.array(x), U)
+    return XY[:,:nx], XY[:,nx:]
 
 umin = -10.
 umax = 10.
 nu = 1 # input dimension
 N_mpc = 10 # MPC horizon length
 nx = 3 # number of states
-U = np.random.rand(N,nu*N_mpc)*(umax-umin)+umin # input sequences
+U = np.random.rand(N+N_mpc,nu)*(umax-umin)+umin # input excitation
+UOLD = np.vstack((np.zeros((1,nu)),U[:-1-N_mpc,:])) # previous input
 R = np.random.randn(N,1) # output references
-X0 = np.random.randn(N,nx) # current states
-UOLD = np.random.rand(N,nu)*(umax-umin)+umin # previous input
-Y = loss(U, X0, R, UOLD)
+x0 = np.zeros((nx,1))
+X0, Y = prediction_with_states(x0, U) # get state trajectories and use them as initial states
+X0 = np.vstack((np.zeros((1,nx)),X0[:-1,:]))[:N] # initial states
+INT = np.random.randn(N,1) # integral action value @each MPC execution step
+#INT = np.cumsum(Y[:N,:]-R).reshape(-1,1) # integral of tracking error
+U = np.squeeze(np.array([U[i:i+N_mpc] for i in range(N)])) # input trajectories
+
+Y = loss(U, X0, R, UOLD, INT)
 #Y = np.arctan(Y) # take arctan to better approximate values where loss is small
 Y, ymean, ygain = standard_scale(Y.reshape(-1,1))
 
-P = np.hstack((X0,R,UOLD))
+P = np.hstack((X0,R,UOLD, INT))
 npar = P.shape[1] # number of states + output references + previous input
 
-pcf = PCF(widths_variable=[10,10], widths_parameter=[10,10], activation_variable='relu', activation_parameter='relu')
-stats = pcf.fit(Y, U, P, tau_th=tau_th, zero_coeff=zero_coeff, cores=cores, seeds=seeds, adam_epochs=1000, lbfgs_epochs=2000)
+pcf = PCF(widths_variable=[10,10], widths_parameter=[10,10], activation_variable='logistic', activation_parameter='swish')
+stats = pcf.fit(Y, U, P, rho_th=1.e-3, tau_th=tau_th, zero_coeff=zero_coeff, cores=cores, seeds=seeds, adam_epochs=1000, lbfgs_epochs=2000)
 
 f_jax, weights = pcf.tojax() # get the jax function and parameters: y = f_jax(x,theta,params)
 YHAT = f_jax(U, P, weights) # predict the output for the training data
@@ -93,8 +109,8 @@ print(f"R2 score on (u,p) -> y mapping:         {stats['R2']}")
 # #########################
 # Convexity check in CVXPY
 x_cvx = cp.Variable((N_mpc*nu, 1))
-theta_cvx = cp.Parameter((npar, 1))
-f_cvx = pcf.tocvxpy(x_cvx, theta_cvx)
+#theta_cvx = cp.Parameter((npar, 1))
+f_cvx, param_cvx, transform_param = pcf.tocvxpy(x_cvx)
 print(f'cvxpy expressions is {"DCP" if f_cvx.is_dcp() else "non-DCP"}')
 print(f'cvxpy expressions is {"DPP" if f_cvx.is_dpp() else "non-DPP"}')
 # #########################
@@ -104,7 +120,7 @@ constr = [x_cvx<=umax*np.ones((N_mpc*nu,1)),
 cvx_prob = cp.Problem(cp.Minimize(f_cvx), constr)
 
 def solve_cvx_problem(cvx_prob, p):
-    theta_cvx.value = np.array(p).reshape(npar, 1)
+    param_cvx.value = transform_param(np.array(p).reshape(npar, 1))
     cvx_prob.solve(solver=cp.SCS)
     return x_cvx.value
 
@@ -131,16 +147,16 @@ options={'iprint': -1, 'maxls': 20, 'gtol': tol, 'eps': 1.e-8,
 Umin=umin*np.ones((N_mpc,nu))
 Umax=umax*np.ones((N_mpc,nu))
 
-def nlmpc(x,r,Uold):
+def nlmpc(x,r,Uold, integral):
     uold=Uold[0]
     U0=np.vstack((Uold[1:],Uold[-1,:])) # initial guess
     solver=jaxopt.ScipyBoundedMinimize(fun=loss_single, tol=tol, maxiter=1000, options=options)
-    Uopt, status = solver.run(U0.reshape(-1), bounds=(Umin.reshape(-1),Umax.reshape(-1)), x0=x, r=r, uold=uold)
+    Uopt, status = solver.run(U0.reshape(-1), bounds=(Umin.reshape(-1),Umax.reshape(-1)), x0=x, r=r, uold=uold, integral=integral)
     del options["maxiter"]
     return Uopt.reshape(N_mpc,nu)
 
-def convexmpc(x,r,Uold):
-    Uopt = solve_cvx_problem(cvx_prob, np.hstack((x,r,Uold[0,:])))
+def convexmpc(x,r,Uold, integral):
+    Uopt = solve_cvx_problem(cvx_prob, np.hstack((x,r,Uold[0,:],integral)))
     return Uopt.reshape(N_mpc,nu)
 
 def closedLoopSimulation(x0,R,controller):
@@ -150,14 +166,16 @@ def closedLoopSimulation(x0,R,controller):
     X=np.empty((T,nx))
     U=np.empty((T,nu))
     x=x0
+    integral=0.
     for k in tqdm.tqdm(range(T)):
-        Uopt=controller(x,R[k],Uold)
+        Uopt=controller(x,R[k],Uold,integral)
         xnext, Y[k] = prediction_model(x, Uopt[0])
         U[k] = Uopt[0]
         if k<T-1:
             X[k+1] = xnext.ravel()
             Uold = Uopt
             x = xnext.ravel()
+            integral += Y[k]-R[k]
     return Y,X,U
 
 print("Simulating closed-loop NLMPC system:")
